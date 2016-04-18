c5948e0b3c912fbfc970d85fcde6dc8e7a25e308
Henry Johnson <nzhenry@gmail.com>
2016-04-18 17:04:16 -0400

    Updated gitignore and webdriver config

--- test/webdriver.js
+++ test/webdriver.js
@@ -1,7 +1,7 @@
 var options = {
 	desiredCapabilities: { browserName: 'firefox' },
-	host: 'starter-selenium-firefox',
-	baseUrl: 'http://starter-tmp:3000'
+	host: 'flags-selenium-firefox',
+	baseUrl: 'http://flags-tmp:3000'
 	//baseUrl: 'http://localhost:3000'
 };
 module.exports = exports = require('webdriverio').remote(options);
